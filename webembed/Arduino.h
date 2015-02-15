4695fac8b016beba66b78d225eb261c914ab3079
David Shah <davey1576@gmail.com>
2015-02-15 11:14:47 +0000

    Add more functions for Arduino compatibility

--- webembed/Arduino.h
+++ webembed/Arduino.h
@@ -105,8 +105,7 @@ uint16_t makeWord(byte h, byte l);
 
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
 
-void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0);
-void noTone(uint8_t _pin);
+
 
 // WMath prototypes
 long random(long);
--- webembed/gpioctl.c
+++ webembed/gpioctl.c
@@ -3,7 +3,7 @@
  * Arduino-style GPIO interface
  * See LICENSE for details
  */
-#include "gpioctl.h"
+#include "Arduino.h"
 #include "gpioint.h"
 #include "gpio.h"
 #include "driver/uart.h"
@@ -78,3 +78,68 @@ void ICACHE_FLASH_ATTR analogWrite(uint8_t pin, int value) {
 	pwm_set_duty(value, pin);
 	pwm_start();
 }
+unsigned ICACHE_FLASH_ATTR long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout) {
+	if(!isValidPin(pin)) return 0;
+    if(pin == 16) return 0; //pin 16 not supported for this function
+    unsigned long waitCount = 0;
+    while((0x1 & GPIO_INPUT_GET(GPIO_ID_PIN(pin))) != state) {
+    	delayMicroseconds(1);
+    	waitCount++;
+    	if(waitCount > timeout) return 0;
+    }
+    unsigned long pulseLength = 0;
+    while((0x1 & GPIO_INPUT_GET(GPIO_ID_PIN(pin))) == state) {
+    	delayMicroseconds(1);
+    	pulseLength++;
+    }
+    return pulseLength;
+}
+void ICACHE_FLASH_ATTR shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val) {
+	if(!isValidPin(dataPin)) return;
+    if(dataPin == 16) return; //pin 16 not supported for this function
+	if(!isValidPin(clockPin)) return;
+    if(clockPin == 16) return; //pin 16 not supported for this function
+    int i;
+    for(i = 0; i < 8; i++) {
+    	int bit;
+        if(bitOrder == MSBFIRST) {
+        	bit = 7 - i;
+        } else {
+        	bit = i;
+        }
+        GPIO_OUTPUT_SET(GPIO_ID_PIN(dataPin),bitRead(val,bit));
+        delayMicroseconds(1);
+        GPIO_OUTPUT_SET(GPIO_ID_PIN(clockPin),HIGH);
+        delayMicroseconds(2);
+        GPIO_OUTPUT_SET(GPIO_ID_PIN(clockPin),LOW);
+        delayMicroseconds(1);
+
+    }
+}
+
+uint8_t ICACHE_FLASH_ATTR shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
+	if(!isValidPin(dataPin)) return 0;
+    if(dataPin == 16) return 0; //pin 16 not supported for this function
+	if(!isValidPin(clockPin)) return 0;
+    if(clockPin == 16) return 0; //pin 16 not supported for this function
+    int i;
+    int val;
+    for(i = 0; i < 8; i++) {
+    	int bit;
+        if(bitOrder == MSBFIRST) {
+        	bit = 7 - i;
+        } else {
+        	bit = i;
+        }
+        delayMicroseconds(1);
+        GPIO_OUTPUT_SET(GPIO_ID_PIN(clockPin),HIGH);
+        delayMicroseconds(1);
+        bitWrite(val, bit, 0x1 & GPIO_INPUT_GET(GPIO_ID_PIN(dataPin)));
+        delayMicroseconds(1);
+        GPIO_OUTPUT_SET(GPIO_ID_PIN(clockPin),LOW);
+        delayMicroseconds(1);
+
+    }
+    return val;
+}
+
